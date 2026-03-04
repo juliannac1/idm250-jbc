b673aa409fae444fa389cb536809ae76a8172ec8
chloe-peng <cp3282@drexel.edu>
2026-03-04 12:11:40 -0500

    Deleted files

--- .env.php
+++ .env.php
@@ -1,9 +1,9 @@
 <?php
     return [
         'DB_HOST' => 'localhost',
-        'DB_NAME' => 'idm250',
-        'DB_USER' => 'root',
-        'DB_PASS' => 'root',
+        'DB_NAME' => 'cp3282_db',
+        'DB_USER' => 'cp3282',
+        'DB_PASS' => 'WZU8qQPgdwXPxmG0',
         'X-API-KEY' => 'jbccms'
     ];
 ?>
